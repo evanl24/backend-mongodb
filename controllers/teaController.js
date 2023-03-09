cb9cc2427072fdf2d96c9828a3f853039723b497
snacccc <evanl24@nycstudents.net>
2023-03-09 13:14:23 -0500

    learning backend

--- controllers/teaController.js
+++ controllers/teaController.js
@@ -1,9 +1,35 @@
+exports.middlewareTest = (req, res, next) => {
+  req.name = 'test';
+  next();
+}
+
 exports.homePage = async (req, res) => {
   const teas = ['Green', 'Black', 'White', 'Oolong'];
   
   try {
+    console.log(req.name);
     res.json(teas);  
   } catch (error) {
     console.log(error);
   }
 };
+
+exports.authMiddleware = async (req, res, next) => {
+  try {
+    if (req.body.user) {
+      next();
+    } else  {
+      res.json('Sign in to continue');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+exports.authPage = async (req, res, next) => {
+  try {
+    res.json(req.body.user);
+  } catch (error) {
+    console.log(error);
+  }
+}
--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const port = process.env.PORT || 3000;
 const app = express();
 const routes = require('./routes/index');
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.use('/', routes);
 
 app.listen(port, () => {
