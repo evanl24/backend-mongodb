458861cda886d5e891ad8a9381638a0078e0fd43
snacccc <evanl24@nycstudents.net>
2023-03-23 13:14:13 -0400

    i hate mongodb

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config({path: 'variables.env'});
+require('dotenv').config({path: 'variables.env' });
 
 mongoose.connect(`${process.env.DATABASE}`, {
   useNewUrlParser: true,
