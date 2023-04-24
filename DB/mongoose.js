57e79ac746882cc8e25f80b6c44d61a90292190c
snacccc <evanl24@nycstudents.net>
2023-04-24 13:10:07 -0400

    aaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAA

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 require('dotenv').config({path: 'variables.env'});
 
 console.log(process.env.DATABASE);
-mongoose.connect(`${process.env.DATABASE}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then (() => console.log('Connected to DB'))
+mongoose.set('strictQuery', true);
+mongoose.connect(`${process.env.DATABASE}`, {})
+.then (() => console.log('Connected to DB'))
 
 mongoose.connection.on('error', (err) => {
   console.log(`${err.message}`)
